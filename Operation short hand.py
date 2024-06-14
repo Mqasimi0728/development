181800a5aaee9c722c495e890ee70a75d6e6e271
Mqasimi0728 <mostafa.qasimi28@gmail.com>
2024-06-14 18:12:08 -0400

    triangle area

---
+++ Operation short hand.py
@@ -0,0 +1,3 @@
+Celsuis=50
+farenhiet=122
+farenhiet=(Celsuis*1.8)+32
---
+++ class1/triangle.py
@@ -0,0 +1,8 @@
+# I want to calculate the area of a triangle
+
+Base=4 #it shows the Base a triangle
+Hieght=3 # it shows the Hieght of triangle
+area=1/2*Base*Hieght # this the triangle formula
+print('triangle area is',area) 
+
+
